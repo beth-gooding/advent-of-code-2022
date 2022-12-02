28b6760fee97ae953411e9cd6d3b3d35663500e3
Beth Gooding <beth.gooding@hotmail.com>
2022-12-02 17:15:11 +0000

    Updates day02 solution to include puzzle 2

--- day02/solution.js
+++ day02/solution.js
@@ -1,6 +1,5 @@
 import * as f from 'fs';
 import * as readline from 'node:readline/promises';
-import { isDeepStrictEqual } from 'util';
 
 const inputFile = './day02/input.txt';
 
@@ -8,31 +7,41 @@ const dayTwoPuzzle = async () => {
     let inputInterface = readline.createInterface({
         input : f.createReadStream(inputFile)
     });
-    let scoreTally = 0;
+    let incorrectScoreTally = 0;
+    let correctScoreTally = 0;
     for await (let roundStrategy of inputInterface) {
         if (roundStrategy[0] === 'A' && roundStrategy[2] === 'X') {
-            scoreTally += (1 + 3);
+            incorrectScoreTally += (1 + 3);
+            correctScoreTally += (3 + 0);
         } else if (roundStrategy[0] === 'A' && roundStrategy[2] === 'Y') {
-            scoreTally += (2 + 6);
+            incorrectScoreTally += (2 + 6);
+            correctScoreTally += (1 + 3);
         }  else if (roundStrategy[0] === 'A' && roundStrategy[2] === 'Z') {
-            scoreTally += (3 + 0);
+            incorrectScoreTally += (3 + 0);
+            correctScoreTally += (2 + 6);
         }  else if (roundStrategy[0] === 'B' && roundStrategy[2] === 'X') {
-            scoreTally += (1 + 0);
+            incorrectScoreTally += (1 + 0);
+            correctScoreTally += (1 + 0);
         }  else if (roundStrategy[0] === 'B' && roundStrategy[2] === 'Y') {
-            scoreTally += (2 + 3);
+            incorrectScoreTally += (2 + 3);
+            correctScoreTally += (2 + 3);
         }  else if (roundStrategy[0] === 'B' && roundStrategy[2] === 'Z') {
-            scoreTally += (3 + 6);
+            incorrectScoreTally += (3 + 6);
+            correctScoreTally += (3 + 6);
         }  else if (roundStrategy[0] === 'C' && roundStrategy[2] === 'X') {
-            scoreTally += (1 + 6);
+            incorrectScoreTally += (1 + 6);
+            correctScoreTally += (2 + 0);
         }  else if (roundStrategy[0] === 'C' && roundStrategy[2] === 'Y') {
-            scoreTally += (2 + 0);
+            incorrectScoreTally += (2 + 0);
+            correctScoreTally += (3 + 3);
         }  else if (roundStrategy[0] === 'C' && roundStrategy[2] === 'Z') {
-            scoreTally += (3 + 3);
+            incorrectScoreTally += (3 + 3);
+            correctScoreTally += (1 + 6);
         } else {
             throw new Error('This strategy option does not follow the rules of the game.')
         }
     }
-    return scoreTally;
+    return [incorrectScoreTally, correctScoreTally];
 }
 
 export default dayTwoPuzzle;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dayTwoPuzzle from "./day02/solution.js";
 
 
 dayOnePuzzle().then((answer) => {
+    console.log()
     console.log("Day one puzzle solutions:");
     console.log(`The elf carrying the most calories has ${Math.max(...answer)} calories`)
     console.log(`The top three elves are carrying a total of ${answer[0] + answer[1] + answer[2]} calories`)
@@ -11,5 +12,6 @@ dayOnePuzzle().then((answer) => {
 dayTwoPuzzle().then((answer) => {
     console.log()
     console.log("Day two puzzle solutions:");
-    console.log(`My total rock-paper-scissors score will be ${answer}`);
+    console.log(`My incorrect total rock-paper-scissors score will be ${answer[0]}`);
+    console.log(`My correct total rock-paper-scissors score will be ${answer[1]}`)
 });

b1348cc3be200c389925ae84637e6803509f8453
Beth Gooding <beth.gooding@hotmail.com>
2022-12-13 21:04:55 +0000

    Adds solution for day 10 puzzle 1

--- day10/solution.js
+++ day10/solution.js
@@ -12,40 +12,22 @@ const dayTenPuzzle = async () => {
     
     let cycle = 1;
     let registerX = 1;
-    let calculateSignalStrengthEarly = false;
 
     for await (let line of inputInterface) {
-        // console.log(cycle, line)
-        let v;
+        let v = 0;
         if (line.startsWith('noop')) {
-            if ([19, 59, 99, 139, 179, 219].indexOf(cycle) !== -1) {
-                calculateSignalStrengthEarly = false;
-            } 
             cycle++;
         } else if (line.startsWith('addx')) {
-            if ([19, 59, 99, 139, 179, 219].indexOf(cycle) !== -1) {
-                calculateSignalStrengthEarly = true;
-            } 
-            v = Number(line.split(' ')[1]);
+            v += Number(line.split(' ')[1]);
             registerX += v;
             cycle += 2;
-
         }
 
         if ([20,60,100,140,180,220].indexOf(cycle) !== -1) {
             sixSignalStrengthsSum += (cycle * registerX);
-            console.log(cycle, registerX, cycle * registerX);
-            console.log(sixSignalStrengthsSum);
 
-        }  else if ([21, 61, 101, 141, 181, 221].indexOf(cycle) !== -1) {
+        } else if ([21, 61, 101, 141, 181, 221].indexOf(cycle) !== -1) {
             sixSignalStrengthsSum += ((cycle - 1) * (registerX - v));
-            console.log(cycle, registerX, cycle * registerX);
-            console.log('bob')
-        } else if (calculateSignalStrengthEarly) {
-            sixSignalStrengthsSum += ((cycle + 1) * registerX);
-            console.log(sixSignalStrengthsSum);
-            console.log(cycle, registerX, (cycle + 1) * registerX);
-            calculateSignalStrengthEarly = false;
         }
 
         if (cycle >= 220) {
